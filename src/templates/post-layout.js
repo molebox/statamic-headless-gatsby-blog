783e87b9a7934a336efe02fec9d2eaffc36e1c8b
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-10-07 19:27:15 +0200

    added footer for links

--- src/templates/post-layout.js
+++ src/templates/post-layout.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { graphql, Link as GatsbyLink } from "gatsby";
-import { Container, Text, Image, Link } from "@chakra-ui/core";
+import { Container, Text, Image, Link, Flex } from "@chakra-ui/core";
 
 const PostLayout = ({ data, pageContext }) => {
   const { title, content, images } = data.collectionPosts;
   const { permalink } = images[0];
   const { previous, next } = pageContext;
-  console.log({ previous });
-  console.log({ next });
+
   return (
     <Container maxW="100ch" centerContent>
       <Text as="h1" fontSize="2xl" my={5}>
@@ -15,17 +14,21 @@ const PostLayout = ({ data, pageContext }) => {
       </Text>
       <Image my={6} src={permalink} alt={title} />
       <Text as="p" dangerouslySetInnerHTML={{ __html: content }} />
-      {previous && (
-        <Link as={GatsbyLink} to={`/${previous.slug}`}>
-          Previous: {previous.title}
-        </Link>
-      )}
+      <Flex mt={6} w="100%" borderTop="solid 1px" p={3}>
+        <Flex w="100ch" justify="space-evenly">
+          {previous && (
+            <Link as={GatsbyLink} to={`/${previous.slug}`}>
+              Previous: {previous.title}
+            </Link>
+          )}
 
-      {next && (
-        <Link as={GatsbyLink} to={`/${next.slug}`}>
-          Next: {next.title}
-        </Link>
-      )}
+          {next && (
+            <Link as={GatsbyLink} to={`/${next.slug}`}>
+              Next: {next.title}
+            </Link>
+          )}
+        </Flex>
+      </Flex>
     </Container>
   );
 };
